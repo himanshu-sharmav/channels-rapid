bd6248ea11c709b88c2196fd86da72560b94e5f5
Himanshu <himan9506492198@gmail.com>
2024-04-04 21:32:44 +0530

    hh

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p&arirqcvxqux&9lddi7a@9g2ziw(%iy)*&4mthpyy@rd9n%e!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
